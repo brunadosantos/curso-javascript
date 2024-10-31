e6f5e1b5b3c6d8328b0c65d0159ef61266a5a3e5
brunadosantos <bluisa286@gmail.com>
2024-10-30 21:56:11 -0300

    exercicio atualizado

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -1,16 +1,20 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('#res')
-    
-    if (fano.value.length = 0 || fano.value > ano) {
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('#res')
+    if (fano.value.length = 0 || fano.value >= ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!');
-    } 
-}
-
-if (fano.value.length = 0 || fano.value > ano) {
-    window.alert('[ERRO] Verifique os dados e tente novamente!');
-} else {
-    window.alert('Tudo certo.')
+    } else {
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - fano.value
+        var gen = ''
+        if (fsex[0].checked) {
+            gen = 'Homem'
+        } else if (fsex[1].checked) {
+            gen = 'Mulher'
+        }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML += `Detectamos ${gen} com ${idade} anos.`
 }
