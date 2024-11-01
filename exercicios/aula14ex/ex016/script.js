cc618827bb3995e19e5293f732ba855ce304221e
brunadosantos <bluisa286@gmail.com>
2024-11-01 18:57:46 -0300

    pasta atualizada

---
+++ exercicios/aula14ex/ex016/script.js
@@ -0,0 +1,7 @@
+function contar() {
+    var Finicio = window.document.getElementById('txtinicio')
+    var Ffim = window.document.getElementById('txtfim')
+    var Fpasso = window.document.getElementById('txtpasso')
+    var resp = window.document.getElementById('res') 
+}
+
