f9a41aca6b6d98b3308fb1c36aa8fc747237f0e0
brunadosantos <bluisa286@gmail.com>
2024-11-05 23:49:01 -0300

    pasta atualizada

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function clicar() {
+    let numero = window.document.getElementById('txtnum')
+    let res = window.document.getElementById('ação')
+
+    let c = Number(numero.value)
+    let r = 0
+
+    for (let n = 1; n <= 10; n++) {
+        r = c * n
+        res.innerHTML += `${c} x ${n} = ${r} <br>`
+    }
+}
